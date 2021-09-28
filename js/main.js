4b4718cbbb654acc3faebf98cd3681a8a05ad0c8
adilet20 <75817697+adilet20@users.noreply.github.com>
2021-09-27 23:10:18 -0400

    Исправлять ошибки

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
-// eslint-disable-next-line no-unused-vars
-const random = function getRandomIntInclusive(min, max) {
+function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-};
+}
 
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random  ссылку на источник!
 
 // eslint-disable-next-line no-unused-vars
-const newRando = function randomNumber(min, max) {
-  return Math.random() * (max - min) + min;
-};
+function getRandomFloat(from ,to, digits) {
+  const main = getRandomInt(from, to);
+  let float = '';
 
-Math.floor(Math.random() * 9) - 10;
+  // eslint-disable-next-line id-length
+  for (let i = 0; i < digits; i++) {
+    float += getRandomInt(1, 9);
+  }
 
+
+  // eslint-disable-next-line prefer-template
+  const result =  main + '.' + float;
+  return parseFloat(result);
+}
 
