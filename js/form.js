5472554448a851d432d1ffb510448179173c5496
adilet20 <75817697+adilet20@users.noreply.github.com>
2021-11-02 22:53:47 -0400

    Правда или действие (часть 1)

--- js/form.js
+++ js/form.js
@@ -19,3 +19,90 @@ disableForm(adForm);
 disableForm(mapFilters);
 activateForm(adForm);
 activateForm(mapFilters);
+
+
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+
+const MAX_CAPACITY = 0;
+const MAX_ROOMS = 100;
+
+
+const PRICES_OF_TYPES = {
+  bungalow: '0',
+  flat: '1000',
+  hotel: '3000',
+  house: '5000',
+  palace: '10000',
+};
+
+const adTitle = adForm.querySelector('#title');
+const adPrice = adForm.querySelector('#price');
+const adType = adForm.querySelector('#type');
+const adCapacity = adForm.querySelector('#capacity');
+const adRooms = adForm.querySelector('#room_number');
+const adTimeIn = adForm.querySelector('#timein');
+const adTimeOut = adForm.querySelector('#timeout');
+const formSubmit = document.querySelector('.ad-form__submit');
+
+
+const  onPriceInput = () => {
+  if (adPrice.validity.valueMissing) {
+    return adPrice.setCustomValidity('Пожалуйста, введите цену');
+  } else if (adPrice.value < PRICES_OF_TYPES[adType.value]) {
+    return adPrice.setCustomValidity(`Минимальная цена ${  PRICES_OF_TYPES[adType.value] } руб. за ночь`);
+  }
+  return adPrice.setCustomValidity('');
+};
+
+const checkCapacity = () => {
+  if (adRooms.value ===  String(MAX_ROOMS) && adCapacity.value !==  String( MAX_CAPACITY)) {
+    return adCapacity.setCustomValidity('Выберите другой вариант');
+  } else if (adRooms.value !==  String(MAX_ROOMS) && adCapacity.value ===  String( MAX_CAPACITY)) {
+    return adCapacity.setCustomValidity('Выберите другой вариант');
+  } else if (adCapacity.value > adRooms.value) {
+    return adCapacity.setCustomValidity('Выберите другой вариант');
+  }
+  return adCapacity.setCustomValidity('');
+};
+
+adTitle.addEventListener('input', () => {
+  const valueLength = adTitle.value.length;
+
+  if (valueLength < MIN_TITLE_LENGTH) {
+    adTitle.setCustomValidity(`Ещё ${  MIN_TITLE_LENGTH - valueLength } симв.`);
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    adTitle.setCustomValidity(`Удалите лишние ${  valueLength - MAX_TITLE_LENGTH } симв.`);
+  } else {
+    adTitle.setCustomValidity('');
+  }
+
+  adTitle.reportValidity();
+});
+
+adType.addEventListener('change', () => {
+  adPrice.placeholder=`${  PRICES_OF_TYPES[adType.value] }`;
+});
+
+adPrice.addEventListener('input', () => {
+  onPriceInput();
+  adPrice.reportValidity();
+});
+
+adCapacity.addEventListener('change', () => {
+  checkCapacity();
+  adCapacity.reportValidity();
+});
+
+adTimeIn.addEventListener('change', () => {
+  adTimeOut.value = adTimeIn.value;
+});
+
+adTimeOut.addEventListener('change', () => {
+  adTimeIn.value = adTimeOut.value;
+});
+
+formSubmit.addEventListener('click', () => {
+  onPriceInput();
+  checkCapacity();
+});
